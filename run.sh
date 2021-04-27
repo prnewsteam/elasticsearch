0f58f91af9cbd981e23da47848e7754b6a229766
Valeriy <valeriy.osipov.b2b@gmail.com>
2021-04-27 18:25:53 +0300

    update config

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ if [ "${CURRENT_ENV}" == "" ]; then
 fi
 
 cp .env.${CURRENT_ENV} .env
+
+sudo sysctl -w vm.max_map_count=262144
 mkdir data/01 data/02 data/03
 sudo chmod 777 data/01 data/02 data/03
 
